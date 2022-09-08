d60972bc3023b7af474c5fcaa07fb339c404591e
endlmk <1269467+endlmk@users.noreply.github.com>
2022-09-08 23:25:17 +0900

    Passed test "VI".

--- src/test/scala/RomanToDecimalTest.scala
+++ src/test/scala/RomanToDecimalTest.scala
@@ -2,15 +2,18 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class RomanToDecimalTest extends AnyFunSuite {
   test("Iを渡すと1を返す") {
-    assert(1 == RomanToDecimal.convert("I"))
+    assert(RomanToDecimal.convert("I") == 1)
   }
   test("IIを渡すと2を返す") {
-    assert(2 == RomanToDecimal.convert("II"))
+    assert(RomanToDecimal.convert("II") == 2)
   }
   test("Vを渡すと5を返す") {
-    assert(5 == RomanToDecimal.convert("V"))
+    assert(RomanToDecimal.convert("V") == 5)
   }
   test("IVを渡すと4を返す") {
-    assert(4 == RomanToDecimal.convert("IV"))
+    assert(RomanToDecimal.convert("IV") == 4)
+  }
+  test("VIを渡すと6を返す") {
+    assert(RomanToDecimal.convert("VI") == 6)
   }
 }
